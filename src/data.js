f29d82bd30bc606585da94a9c83dbe1b3839473e
Col-R <2colewestin@gmail.com>
2022-08-26 14:32:11 -0500

    data file completed

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const Data = [
+    {
+        id: 1,
+        title: "Kapolei",
+        location: "Oahu, Hawaii",
+        googleMapsUrl: "https://goo.gl/maps/cmGx35RpwnmWZEea9",
+        isPlayed: true,
+        description: "Designed by architect Ted Robinson, who is best known for his use of water features and referred to as the 'King of Waterscapes', which is very visible at Kapolei",
+        imageUrl: "https://kapoleigolf.com/wp-content/uploads/2015/11/2.jpg"
+    },
+    {
+        id: 2,
+        title: "Superior National",
+        location: "Lutsen, MN",
+        googleMapsUrl: "https://goo.gl/maps/5tcPNyygzNFdqknN8",
+        isPlayed: true,
+        description: "Located on the North Shore of Lake Superior, Superior National provides some of the most dramatic landscapes and challenging golf in all of the Midwest,",
+        imageUrl: "https://www.superiornational.com/wp-content/uploads/sites/8319/2021/09/Hole-2-C2711-2.jpg"
+    },
+    {
+        id: 3,
+        title: "The Stadium Course - TPC Sawgrass",
+        location: "Ponte Vedra Beach, FL",
+        googleMapsUrl: "https://goo.gl/maps/bB4NLjW4Q5X34TXF9",
+        isPlayed: false,
+        description: "Home of The Players Championship, Pete Dye's Stadium Course is one of the world's most iconic tracks. The signature island green on the par-3, 17th hole is one of the most recognized in golf and the course is consistently named among the best in the world.",
+        imageUrl: "https://tpc.com/sawgrass/wp-content/uploads/sites/51/2021/10/19_PLA_0307_00345_CCp-1024x683.jpg"
+    },
+    {
+        id: 4,
+        title: "Royal Dornoch",
+        location: "Dornoch, Scotland",
+        googleMapsUrl: "https://goo.gl/maps/bVHV6aR3qZ9ChUkU8",
+        isPlayed: false,
+        description: "Royal Dornoch is spellbinding and many golfers from all over the world make the pilgrimage to this natural links at some point in their lives. It is often quoted as one of the must-play courses.",
+        imageUrl: "https://www.linksmagazine.com/wp-content/uploads/2021/06/Royal-Dornoch-feat.jpg"
+    },
+    {
+        id: 5,
+        title: "The Old Course at St. Andrews",
+        location: "St. Andrews, UK",
+        googleMapsUrl: "https://goo.gl/maps/zxDmbUMAz2PijgGd6",
+        isPlayed: false,
+        description: "The oldest and most iconic golf course in the world. The Swilcan Bridge and Hell Bunker are recognised across the globe, yet the greatest feature of the Old Course is that despite its grand status it remains a public golf course, open to all.",
+        imageUrl: "https://pga-tour-res.cloudinary.com/image/upload/c_fill,f_auto,g_center,h_478,q_auto,w_850/v1/pgatour/editorial/2022/07/10/standrews-18-1694-davidcannon.jpg"
+    },
+]
