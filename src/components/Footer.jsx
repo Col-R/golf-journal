934732389b3ce63a17cf6ff05a96b0904eb25e7e
Col-R <2colewestin@gmail.com>
2022-08-27 13:45:42 -0500

    updated github link to direct to this repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     <footer className="footer">
         <p className='footer__creator'>Made by Cole Robinson</p>
 
-        <a href="https://github.com/Col-R" target='_blank' rel="noreferrer"><img src = {github} alt = "github" className = "social__icon"/></a>
+        <a href="https://github.com/Col-R/golf-journal" target='_blank' rel="noreferrer"><img src = {github} alt = "github" className = "social__icon"/></a>
         <a href="https://www.linkedin.com/in/cwrobinson-/" target ='_blank' rel="noreferrer"><img src = {linkedIn} alt = "linkedIn" className = "social__icon" /></a>
         
         
