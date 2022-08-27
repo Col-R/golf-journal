a2370e1f352090356d8db9941f86ae66e3c1f681
Col-R <2colewestin@gmail.com>
2022-08-27 13:19:35 -0500

    finidhed media queries for tablet and mobile

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,7 @@ const Card = (props) => {
                 <img className = "card__img" src={props.course.imageUrl} alt="golf course"/>
             </div>
             <div className = "card__right">
-                <p className="card__location"> <img id = "map__icon" src = {map_icon} alt = "map"/>{props.course.location} | <a href={props.course.googleMapsUrl} className="card__mapsUrl" target="_blank" rel="noreferrer">View on Google Maps</a></p>
+                <p className="card__location"> <img id = "map__icon" src = {map_icon} alt = "map"/>{props.course.location} <span>|</span> <a href={props.course.googleMapsUrl} className="card__mapsUrl" target="_blank" rel="noreferrer">View on Google Maps</a></p>
                 <h1 className="card__title">{props.course.title}</h1>
                 {/*Only displays if isPlayed == true in data.js*/}
                 {props.course.isPlayed && <p className="card__played">Played <img src = {checkmark} alt = "checkmark" id="checkmark"/></p> }
