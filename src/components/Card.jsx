30631ed127b16e02d0d540cc988d1a0966f622a5
Col-R <2colewestin@gmail.com>
2022-08-27 12:24:24 -0500

    alignment and starter styling for cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
+import './Card.css'
+
+// icon
+
+import map_icon from '../img/map-icon.png'
+import checkmark from '../img/checkmark.png'
+
 const Card = (props) => {
-  return (
-    <div>Card</div>
+
+    return (
+        <section class = "card">
+            <div className = "card__left">
+                <img class = "card__img" src={props.course.imageUrl} alt="golf course"/>
+            </div>
+            <div className = "card__right">
+                <p className="card__location"> <img id = "map__icon" src = {map_icon} alt = "map"/>{props.course.location} <a href={props.course.googleMapsUrl} className="card__mapsUrl" target="_blank" rel="noreferrer">View on Google Maps</a></p>
+                <h1 className="card__title">{props.course.title}</h1>
+                {/*Only displays if isPlayed == true in data.js*/}
+                {props.course.isPlayed && <p className="card__played">Played <img src = {checkmark} alt = "checkmark" id="checkmark"/></p> }
+                <p className="description">{props.course.description}</p>
+            </div>
+        </section>
   )
 }
 
